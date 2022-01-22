4fb58f56fbe823e9a537639dd2e0375e388f5391
ANSHID <93968310+ANUSER1@users.noreply.github.com>
2022-01-22 16:13:53 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -10,19 +10,15 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: "Gives github link of the
     var skl = await axios.get("https://i.imgur.com/Mbaz2g9.jpg", { responseType: 'arraybuffer' })
 
     await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*spirit  bot created by Spirit*
-*Creator number : wa.me/18052196558?text=Hi%20spirit%20.%20*
+*Creator number : wa.me/916238560742?text=Hi%20𝒂𝒏𝒔𝒉𝒊𝒅%20.%20*
 
-*spirit bot group* : https://chat.whatsapp.com/F3adhxvYpd3KzVptzDxhC4
+*Pᴜʙʟɪᴄ ʙᴏᴛ ɢʀᴏᴜᴘ* : https://chat.whatsapp.com/F3adhxvYpd3KzVptzDxhC4
 
-*Githublink (Setup)* :    https://github.com/SPlRlT-YT/spirit-ser
+*Gɪᴛʜᴜʙ ʟɪɴᴋ * :    https://github.com/ANUSER1/COSMO
 
-*Audio commads* :   https://github.com/SPlRlT-YT/spirit-ser/tree/master/uploads
+*Aᴜᴅɪᴏ ᴄᴀᴍᴍᴀɴᴅs* :   https://github.com/ANUSER1/COSMO/tree/master/uploads
 
-*Sticker commads* : https://github.com/SPlRlT-YT/spirit-ser/tree/master/sticker
-
-*My channel* : https://youtube.com/channel/UCBv1Z1zkOmdGeWU2QRdYBnA 
-
-*For More Updates Subscribe The Channel ☝*
+*Sᴛɪᴄᴋᴇʀ ᴄᴀᴍᴍᴀɴᴅs* : https://github.com/QNUSER1/COSMO/tree/master/sticker
 `}) 
 
 }));
